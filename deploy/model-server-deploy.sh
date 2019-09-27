8adff7bf4fc88d2075af17ba819845e057d5b559
Paul Schlueter <paul@paulschlueter.com>
2019-09-27 09:16:17 -0500

    Created Step Functions Initiation Lambda

--- deploy/model-server-deploy.sh
+++ deploy/model-server-deploy.sh
@@ -4,6 +4,7 @@ aws cloudformation create-stack \
     --stack-name $STACK_NAME \
     --template-body file://model-server.yaml \
     --capabilities CAPABILITY_IAM \
-    --parameters file://model-server-parameters.json
+    --parameters file://model-server-parameters.json \
+    --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
