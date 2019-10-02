a59945c48ccc42b4ac3fc28247fa5a1d2a3438d4
Paul Schlueter <paul@paulschlueter.com>
2019-10-02 10:38:49 -0500

    Move Lambdas to Separate Files
    (not inline in the CF template)

--- deploy/model-server-deploy.sh
+++ deploy/model-server-deploy.sh
@@ -1,10 +1,27 @@
 STACK_NAME=model-server-pipeline
+DEPLOYMENT_BUCKET=cf-deploy-1eeb111769f855e024546b6f5d94be21
 
-aws cloudformation create-stack \
+# Create Cloudformation deployment S3 bucket if it doesn't exist
+if aws s3api head-bucket --bucket "$DEPLOYMENT_BUCKET" 2>/dev/null; then
+    echo S3 bucket $DEPLOYMENT_BUCKET already exists. Not creating it.
+else
+    aws s3api create-bucket --bucket $DEPLOYMENT_BUCKET
+    echo Created S3 bucket: $DEPLOYMENT_BUCKET.
+fi
+
+# Package and upload the lambdas and create the modified Cloudformation template
+aws cloudformation package \
+    --template-file model-server.yaml \
+    --output-template-file model-server.packaged.yaml \
+    --s3-bucket $DEPLOYMENT_BUCKET
+
+# Deploy the modified Cloudformation template
+aws cloudformation update-stack \
     --stack-name $STACK_NAME \
-    --template-body file://model-server.yaml \
-    --capabilities CAPABILITY_IAM \
+    --template-body file://model-server.packaged.yaml \
     --parameters file://model-server-parameters.json \
     --capabilities CAPABILITY_NAMED_IAM
 
-aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+aws cloudformation wait stack-update-complete --stack-name $STACK_NAME
+
+rm model-server.packaged.yaml
